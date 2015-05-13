25257a8b1a9c1a2d5c5147f11b220fe9875de7e4
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-05-13 13:51:08 +0200

    Save the data during the run

--- include/cpm/cpm.hpp
+++ include/cpm/cpm.hpp
@@ -27,19 +27,32 @@ inline void call_with_data(Tuple& data, Functor functor, std::index_sequence<I..
 template<typename DefaultPolicy = std_stop_policy>
 struct benchmark;
 
-template<typename Bench, typename Policy>
-struct section {
-private:
+struct section_data {
     std::string name;
-    Bench& bench;
 
     //TODO This datastructure is probably not ideal
     std::vector<std::string> names;
     std::vector<std::size_t> sizes;
     std::vector<std::vector<std::size_t>> results;
 
+    section_data() = default;
+    section_data(const section_data&) = default;
+    section_data& operator=(const section_data&) = default;
+    section_data(section_data&&) = default;
+    section_data& operator=(section_data&&) = default;
+};
+
+template<typename Bench, typename Policy>
+struct section {
+private:
+    Bench& bench;
+
+    section_data data;
+
 public:
-    section(std::string name, Bench& bench) : name(std::move(name)), bench(bench) {}
+    section(std::string name, Bench& bench) : bench(bench) {
+        data.name = std::move(name);
+    }
 
     //Measure simple functor (no randomization)
 
@@ -82,66 +95,66 @@ public:
 
     ~section(){
         if(bench.standard_report){
-            if(names.empty()){
+            if(data.names.empty()){
                 return;
             }
 
-            std::vector<int> widths(1 + results.size(), 4);
+            std::vector<int> widths(1 + data.results.size(), 4);
 
-            for(std::size_t i = 0; i < sizes.size(); ++i){
-                auto s = sizes[i];
+            for(std::size_t i = 0; i < data.sizes.size(); ++i){
+                auto s = data.sizes[i];
 
                 widths[0] = std::max(widths[0], static_cast<int>(std::to_string(s).size()));
             }
 
-            widths[0] = std::max(widths[0], static_cast<int>(name.size()));
+            widths[0] = std::max(widths[0], static_cast<int>(data.name.size()));
 
-            for(std::size_t i = 0; i < results.size(); ++i){
-                for(auto d : results[i]){
+            for(std::size_t i = 0; i < data.results.size(); ++i){
+                for(auto d : data.results[i]){
                     widths[i+1] = std::max(widths[i+1], static_cast<int>(us_duration_str(d).size()));
                 }
             }
 
-            for(std::size_t i = 0; i < names.size(); ++i){
-                widths[i+1] = std::max(widths[i+1], static_cast<int>(names[i].size()));
+            for(std::size_t i = 0; i < data.names.size(); ++i){
+                widths[i+1] = std::max(widths[i+1], static_cast<int>(data.names[i].size()));
             }
 
-            std::size_t tot_width = 1 + std::accumulate(widths.begin(), widths.end(), 0) + 3 * (1 + names.size());
+            std::size_t tot_width = 1 + std::accumulate(widths.begin(), widths.end(), 0) + 3 * (1 + data.names.size());
 
             std::cout << " " << std::string(tot_width, '-') << std::endl;;
 
-            printf(" | %*s | ", widths[0], name.c_str());
-            for(std::size_t i = 0; i < names.size(); ++i){
-                printf("%*s | ", widths[i+1], names[i].c_str());
+            printf(" | %*s | ", widths[0], data.name.c_str());
+            for(std::size_t i = 0; i < data.names.size(); ++i){
+                printf("%*s | ", widths[i+1], data.names[i].c_str());
             }
             printf("\n");
 
             std::cout << " " << std::string(tot_width, '-') << std::endl;;
 
-            for(std::size_t i = 0; i < sizes.size(); ++i){
-                auto s = sizes[i];
+            for(std::size_t i = 0; i < data.sizes.size(); ++i){
+                auto s = data.sizes[i];
 
                 printf(" | %*ld | ", widths[0], s);
 
                 std::size_t max = 0;
                 std::size_t min = std::numeric_limits<int>::max();
 
-                for(std::size_t r = 0; r < results.size(); ++r){
-                    if(i < results[r].size()){
-                        max = std::max(max, results[r][i]);
-                        min = std::min(min, results[r][i]);
+                for(std::size_t r = 0; r < data.results.size(); ++r){
+                    if(i < data.results[r].size()){
+                        max = std::max(max, data.results[r][i]);
+                        min = std::min(min, data.results[r][i]);
                     }
                 }
 
-                for(std::size_t r = 0; r < results.size(); ++r){
-                    if(i < results[r].size()){
-                        if(results[r][i] == min){
+                for(std::size_t r = 0; r < data.results.size(); ++r){
+                    if(i < data.results[r].size()){
+                        if(data.results[r][i] == min){
                             std::cout << "\033[0;32m";
-                        } else if(results[r][i] == max){
+                        } else if(data.results[r][i] == max){
                             std::cout << "\033[0;31m";
                         }
 
-                        printf("%*s", widths[r+1], us_duration_str(results[r][i]).c_str());
+                        printf("%*s", widths[r+1], us_duration_str(data.results[r][i]).c_str());
                     } else {
                         std::cout << "\033[0;31m";
                         printf("%*s", widths[r+1], "*");
@@ -156,21 +169,28 @@ public:
 
             std::cout << " " << std::string(tot_width, '-') << std::endl;;
         }
+
+        bench.section_results.push_back(std::move(data));
     }
 
 private:
     void report(const std::string& title, std::size_t d, std::size_t duration){
-        if(names.empty() || names.back() != title){
-            names.push_back(title);
-            results.emplace_back();
+        if(data.names.empty() || data.names.back() != title){
+            data.names.push_back(title);
+            data.results.emplace_back();
         }
 
-        if(names.size() == 1){
-            sizes.push_back(d);
+        if(data.names.size() == 1){
+            data.sizes.push_back(d);
         }
 
-        results.back().push_back(duration);
+        data.results.back().push_back(duration);
     }
+};
+
+struct measure_data {
+    std::string title;
+    std::vector<std::pair<std::size_t, std::size_t>> results;
 };
 
 template<typename DefaultPolicy>
@@ -182,6 +202,9 @@ private:
     std::size_t tests = 0;
     std::size_t measures = 0;
     std::size_t runs = 0;
+
+    std::vector<measure_data> results;
+    std::vector<section_data> section_results;
 
 public:
     std::size_t warmup = 10;
@@ -219,39 +242,57 @@ public:
 
     template<typename Policy = DefaultPolicy, typename Functor>
     void measure_simple(const std::string& title, Functor functor){
+        measure_data data;
+        data.title = title;
+
         policy_run<Policy>(
-            [&title, &functor, this](std::size_t d){
+            [&data, &title, &functor, this](std::size_t d){
                 auto duration = measure_only_simple(functor, d);
                 report(title, d, duration);
+                data.results.push_back(std::make_pair(d, duration));
                 return duration;
             }
         );
+
+        results.push_back(std::move(data));
     }
 
     //Measure with two-pass functors (init and functor)
 
     template<typename Policy = DefaultPolicy, typename Init, typename Functor>
     void measure_two_pass(const std::string& title, Init init, Functor functor){
+        measure_data data;
+        data.title = title;
+
         policy_run<Policy>(
-            [&title, &functor, &init, this](std::size_t d){
+            [&data, &title, &functor, &init, this](std::size_t d){
                 auto duration = measure_only_two_pass(init, functor, d);
                 report(title, d, duration);
+                data.results.push_back(std::make_pair(d, duration));
                 return duration;
             }
         );
+
+        results.push_back(std::move(data));
     }
 
     //measure a function with global references
 
     template<typename Policy = DefaultPolicy, typename Functor, typename... T>
     void measure_global(const std::string& title, Functor functor, T&... references){
+        measure_data data;
+        data.title = title;
+
         policy_run<Policy>(
-            [&title, &functor, &references..., this](std::size_t d){
+            [&data, &title, &functor, &references..., this](std::size_t d){
                 auto duration = measure_only_global(functor, d, references...);
                 report(title, d, duration);
+                data.results.push_back(std::make_pair(d, duration));
                 return duration;
             }
         );
+
+        results.push_back(std::move(data));
     }
 
     //Measure and return the duration of a simple functor
