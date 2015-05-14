31406e4cfda7d7798d973810e0d475f4e339fa32
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-05-14 21:02:37 +0200

    Easy way to generate different data

--- src/simple.cpp
+++ src/simple.cpp
@@ -9,6 +9,8 @@
 
 #include <thread>
 
+constexpr const double factor = 0.8;
+
 constexpr std::chrono::nanoseconds operator ""_ns(unsigned long long us){
     return std::chrono::nanoseconds(us);
 }
@@ -21,30 +23,30 @@ int main(){
 
     bench.begin();
 
-    bench.measure_simple("simple_a", [](std::size_t d){ std::this_thread::sleep_for(d * 1_ns ); });
-    bench.measure_simple("simple_b", [](std::size_t d){ std::this_thread::sleep_for(d * 2_ns ); });
+    bench.measure_simple("simple_a", [](std::size_t d){ std::this_thread::sleep_for((factor * d) * 1_ns ); });
+    bench.measure_simple("simple_b", [](std::size_t d){ std::this_thread::sleep_for((factor * d) * 2_ns ); });
 
     test a{3};
     test b{5};
-    bench.measure_global("global_a", [&a](std::size_t d){ std::this_thread::sleep_for(d * a.d * 1_ns ); }, a);
-    bench.measure_global("global_b", [&b](std::size_t d){ std::this_thread::sleep_for(d * b.d * 1_ns ); }, b);
+    bench.measure_global("global_a", [&a](std::size_t d){ std::this_thread::sleep_for((factor * d * a.d) * 1_ns ); }, a);
+    bench.measure_global("global_b", [&b](std::size_t d){ std::this_thread::sleep_for((factor * d * b.d) * 1_ns ); }, b);
 
     bench.measure_two_pass("2p_a",
         [](std::size_t d){ return std::make_tuple(test{d}); },
-        [](std::size_t d, test& d2){ std::this_thread::sleep_for((d + d2.d) * 1_ns ); }
+        [](std::size_t d, test& d2){ std::this_thread::sleep_for((factor * (d + d2.d)) * 1_ns ); }
         );
 
     bench.measure_two_pass("2p_b",
         [](std::size_t d){ return std::make_tuple(test{d}); },
-        [](std::size_t d, test& d2){ std::this_thread::sleep_for(2 * (d + d2.d) * 1_ns ); }
+        [](std::size_t d, test& d2){ std::this_thread::sleep_for((factor * 2 * (d + d2.d)) * 1_ns ); }
         );
 
     {
         auto sec = bench.multi("mmul");
 
-        sec.measure_simple("std", [](std::size_t d){ std::this_thread::sleep_for(d * 9_ns ); });
-        sec.measure_simple("fast", [](std::size_t d){ std::this_thread::sleep_for((d / 3) * 1_ns ); });
-        sec.measure_simple("common", [](std::size_t d){ std::this_thread::sleep_for((d / 2) * 3_ns ); });
+        sec.measure_simple("std", [](std::size_t d){ std::this_thread::sleep_for((factor * d) * 9_ns ); });
+        sec.measure_simple("fast", [](std::size_t d){ std::this_thread::sleep_for((factor * (d / 3)) * 1_ns ); });
+        sec.measure_simple("common", [](std::size_t d){ std::this_thread::sleep_for((factor * (d / 2)) * 3_ns ); });
     }
 
     {
@@ -52,12 +54,12 @@ int main(){
 
         sec.measure_two_pass("std",
             [](std::size_t d){ return std::make_tuple(test{d}); },
-            [](std::size_t d, test& d2){ std::this_thread::sleep_for(5 * (d + d2.d) * 1_ns ); }
+            [](std::size_t d, test& d2){ std::this_thread::sleep_for((factor * 5 * (d + d2.d)) * 1_ns ); }
             );
 
         sec.measure_two_pass("fast",
             [](std::size_t d){ return std::make_tuple(test{d}); },
-            [](std::size_t d, test& d2){ std::this_thread::sleep_for(3 * (d + d2.d) * 1_ns ); }
+            [](std::size_t d, test& d2){ std::this_thread::sleep_for((factor * 3 * (d + d2.d)) * 1_ns ); }
             );
     }
 
@@ -66,7 +68,7 @@ int main(){
 
         test a{3};
         test b{5};
-        sec.measure_global("std", [&a](std::size_t d){ std::this_thread::sleep_for(d * (d % a.d) * 1_ns ); }, a);
-        sec.measure_global("mkl", [&b](std::size_t d){ std::this_thread::sleep_for(d * (d % b.d) * 1_ns ); }, b);
+        sec.measure_global("std", [&a](std::size_t d){ std::this_thread::sleep_for((factor * d * (d % a.d)) * 1_ns ); }, a);
+        sec.measure_global("mkl", [&b](std::size_t d){ std::this_thread::sleep_for((factor * d * (d % b.d)) * 1_ns ); }, b);
     }
 }
