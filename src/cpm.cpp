30263b13af4a7f7d3b81320e8c0ce5b02b31704a
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-05-14 15:14:10 +0200

    Prepare source files for the generator

---
+++ src/cpm.cpp
@@ -0,0 +1,14 @@
+//=======================================================================
+// Copyright (c) 2015 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#include <iostream>
+
+int main(){
+    std::string folder = "results";
+
+    return 0;
+}
--- src/simple.cpp
+++ src/simple.cpp
@@ -1,3 +1,10 @@
+//=======================================================================
+// Copyright (c) 2015 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
 #include "cpm/cpm.hpp"
 
 #include <thread>
