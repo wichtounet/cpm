20d9db8e6edb782fa86d972c2b23690d30c29600
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-05-14 15:49:30 +0200

    Integrate rapidjson

--- src/cpm.cpp
+++ src/cpm.cpp
@@ -7,8 +7,41 @@
 
 #include <iostream>
 
+#include "rapidjson/document.h"
+#include "rapidjson/filereadstream.h"
+
+//Allow range-based for loop on rapidjson objects
+
+namespace rapidjson {
+
+inline auto begin(rapidjson::Value& value){
+    return value.Begin();
+}
+
+inline auto end(rapidjson::Value& value){
+    return value.End();
+}
+
+} //end of namespace rapidjson
+
 int main(){
-    std::string folder = "results";
+    std::string file = "results/41.cpm";
+
+    FILE* pFile = fopen(file.c_str(), "rb");
+    char buffer[65536];
+
+    rapidjson::FileReadStream is(pFile, buffer, sizeof(buffer));
+    rapidjson::Document doc;
+    doc.ParseStream<0>(is);
+
+    std::cout << doc["name"].GetString() << std::endl;
+
+    auto& results = doc["results"];
+
+    for(auto& result : results){
+
+        std::cout << result["title"].GetString() << std::endl;
+    }
 
     return 0;
 }
