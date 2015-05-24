d7cae1083317e268e14322b4c4ef3a6e696da662
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-05-24 18:05:24 +0200

    Add support for parallel randomization

--- include/cpm/random.hpp
+++ include/cpm/random.hpp
@@ -10,7 +10,109 @@
 
 #include <random>
 
+#ifdef CPM_PARALLEL_RANDOMIZE
+#include <thread>
+#include <future>
+#endif
+
 namespace cpm {
+
+#ifndef CPM_PARALLEL_THRESHOLD
+#define CPM_PARALLEL_THRESHOLD 10000
+#endif
+
+#ifndef CPM_PARALLEL_THREADS
+//We are assuming Hyper-Threading by default
+#define CPM_PARALLEL_THREADS std::thread::hardware_concurrency() / 2
+#endif
+
+#ifdef CPM_PARALLEL_RANDOMIZE
+
+#ifndef CPM_FAST_RANDOMIZE
+
+template<typename T>
+void randomize_double(T& container){
+    if(container.size() > CPM_PARALLEL_THRESHOLD){
+        const std::size_t n = CPM_PARALLEL_THREADS; //We are assuming Hyper-Threading
+        const std::size_t p = container.size() / n;
+
+        std::vector<std::future<void>> futures;
+
+        for(std::size_t i = 0; i < n; ++i){
+            futures.push_back(std::async(std::launch::async, [&](std::size_t ii){ 
+                static std::random_device rd;
+                static std::mt19937_64 rand_engine(rd());
+                static std::uniform_real_distribution<double> real_distribution(-10000.0, 10000.0);
+                static auto generator = std::bind(real_distribution, rand_engine);
+
+                for(std::size_t j = p * ii; j < p * (ii + 1); ++j){
+                    container[j] = generator();
+                }
+                }, i));
+        }
+
+        static std::random_device rd;
+        static std::mt19937_64 rand_engine(rd());
+        static std::uniform_real_distribution<double> real_distribution(-10000.0, 10000.0);
+        static auto generator = std::bind(real_distribution, rand_engine);
+
+        for(std::size_t j = container.size() - container.size() % p; j < container.size(); ++j){
+            container[j] = generator();
+        }
+    } else {
+        static std::random_device rd;
+        static std::mt19937_64 rand_engine(rd());
+        static std::uniform_real_distribution<double> real_distribution(-10000.0, 10000.0);
+        static auto generator = std::bind(real_distribution, rand_engine);
+
+        for(auto& v : container){
+            v = generator();
+        }
+    }
+}
+
+#else
+
+template<typename T>
+void randomize_double(T& container){
+    static std::random_device rd;
+    static std::mt19937_64 rand_engine(rd());
+    static std::uniform_real_distribution<double> real_distribution(-10000.0, 10000.0);
+    static auto generator = std::bind(real_distribution, rand_engine);
+
+    const auto a = generator();
+    const auto b = generator();
+    const auto c = generator();
+
+    if(container.size() > CPM_PARALLEL_THRESHOLD){
+        const std::size_t n = CPM_PARALLEL_THREADS; //We are assuming Hyper-Threading
+        const std::size_t p = container.size() / n;
+
+        std::vector<std::future<void>> futures;
+
+        for(std::size_t i = 0; i < n; ++i){
+            futures.push_back(std::async(std::launch::async, [&](std::size_t ii){ 
+                for(std::size_t j = p * ii; j < p * (ii + 1); ++j){
+                    container[j] = a + (j * b) + (-j * c);
+                }
+                }, i));
+        }
+
+        for(std::size_t j = container.size() - container.size() % p; j < container.size(); ++j){
+            container[j] = a + (j * b) + (-j * c);
+        }
+    } else {
+        std::size_t i = 0;
+        for(auto& v : container){
+            v = a + (i * b) + (-i * c);
+            ++i;
+        }
+    }
+}
+
+#endif //CPM_FAST_RANDOMIZE
+
+#else
 
 #ifndef CPM_FAST_RANDOMIZE
 
@@ -46,8 +148,9 @@ void randomize_double(T& container){
     }
 }
 
-#endif
+#endif //CPM_FAST_RANDOMIZE
 
+#endif //CPM_PARALLEL_RANDOMIZE
 
 inline void randomize(){}
 
