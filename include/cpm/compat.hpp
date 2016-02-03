c42e436277411a48c5ecd646f79e1a7cc187e663
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-02-03 11:07:11 +0100

    Safety for macros

--- include/cpm/compat.hpp
+++ include/cpm/compat.hpp
@@ -8,18 +8,22 @@
 #ifndef CPM_COMPAT_HPP
 #define CPM_COMPAT_HPP
 
+#ifndef cpp14_constexpr
 #ifdef __clang__
 #define cpp14_constexpr constexpr
 #else
 #define cpp14_constexpr
 #endif
+#endif
 
 //Fix an assertion failed in Intel C++ Compiler
 
+#ifndef intel_decltype_auto
 #ifdef __INTEL_COMPILER
 #define intel_decltype_auto auto
 #else
 #define intel_decltype_auto decltype(auto)
+#endif
 #endif
 
 #endif //CPM_COMPAT_HPP
